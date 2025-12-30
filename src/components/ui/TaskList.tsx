2a3976b129d5f23f6746f5463307561e83e58279
Snir Azran <snirazran1@gmail.com>
2025-12-31 01:02:40 +0200

    fix for moving tags

--- src/components/ui/TaskList.tsx
+++ src/components/ui/TaskList.tsx
@@ -224,7 +224,7 @@ function SortableTaskItem({
 
                       return (
                         <motion.span
-                          layout
+                          layout={!isDragging}
                           initial={{ opacity: 0, scale: 0.8 }}
                           animate={{ opacity: 1, scale: 1 }}
                           exit={{ opacity: 0, scale: 0 }}
