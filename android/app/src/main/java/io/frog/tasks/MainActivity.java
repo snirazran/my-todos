2c0927bb20e8904e62b6d665ac89c6b7f390b6dd
Snir Azran <snirazran1@gmail.com>
2026-02-10 21:29:51 +0200

    making sure i mobile

---
+++ android/app/src/main/java/io/frog/tasks/MainActivity.java
@@ -0,0 +1,5 @@
+package io.frog.tasks;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.frog.tasks',
+  appName: 'daily-todo-tracker',
+  webDir: 'out',
+  server: {
+    url: 'http://10.0.2.2:3000', // Use 10.0.2.2 for Android Emulator. Replace with LAN IP for real device.
+    cleartext: true,
+  },
+};
+
+export default config;
